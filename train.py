e1fd444b05f77781966517e902ebb5a1ee1ec84c
ansh941 <ansh941@rebuilderia.com>
2024-07-11 18:51:48 +0900

    change the loss function from CE to RMSE

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 
 import tqdm
 
-import tensorboard
+import tensorboardX
 
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -62,7 +62,7 @@ def train(vis=False):
     
     # Set hyperparameters
     batch_size = 4
-    num_epochs = 100
+    num_epochs = 20
     learning_rate = 0.001
     
     mean = torch.tensor([0.485, 0.456, 0.406]).to(device).reshape(1,3,1,1)
@@ -81,22 +81,28 @@ def train(vis=False):
     train_dataset = SVABOMaterialDataset(root_dir='../../datasets/abo-benchmark-material', image_transform=image_transform, label_transform=label_transform, train=True)
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     
+    test_dataset = SVABOMaterialDataset(root_dir='../../datasets/abo-benchmark-material', image_transform=image_transform, label_transform=label_transform, train=False)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    
     # Set model
     model = SVNet(activation='relu').to(device)
     
     # Set loss function and optimizer
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
     # Set TensorBoard
-    # writer = tensorboard.SummaryWriter()
+    writer = tensorboardX.SummaryWriter()
     
     light_dir = torch.tensor([0,0,1]).to(device).float()
     view_dir = torch.tensor([0,0,1]).to(device).float()
     
     # Train the model
     total_step = len(train_loader)
-    for epoch in tqdm.tqdm(range(num_epochs)):
+    for epoch in range(num_epochs):
+        total_loss = 0
+        progress_bar = tqdm.tqdm(range(len(train_loader)))
         for i, (render_view, base_color, normal, metallic, roughness, mask, recon_view) in enumerate(train_loader):
             optimizer.zero_grad()
             render_view = render_view.to(device).float()
@@ -127,22 +133,78 @@ def train(vis=False):
             recon_view_pred = recon_view_pred.permute(0, 3, 1, 2)
             
             # Compute loss
-            base_color_loss = criterion(base_color_pred, base_color)
-            normal_loss = criterion(normal_pred, normal)
-            metallic_loss = criterion(metallic_pred, metallic)
-            roughness_loss = criterion(roughness_pred, roughness)
-            rendering_loss = criterion(recon_view_pred, recon_view)
+            base_color_loss = torch.sqrt(criterion(base_color_pred, base_color))
+            normal_loss = torch.sqrt(criterion(normal_pred, normal))
+            metallic_loss = torch.sqrt(criterion(metallic_pred, metallic))
+            roughness_loss = torch.sqrt(criterion(roughness_pred, roughness))
+            rendering_loss = torch.sqrt(criterion(recon_view_pred, recon_view))
             loss = (base_color_loss + normal_loss + metallic_loss + roughness_loss + rendering_loss)/5
             loss.backward()
             optimizer.step()
             
+            total_loss += loss.item()
             if i % 100 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                print('Train - Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                # Visualization
+                if vis:
+                    visualize(render_view, base_color, normal, metallic, roughness, mask, recon_view,
+                            base_color_pred, normal_pred, metallic_pred, roughness_pred, recon_view_pred,
+                            mean, std)
+            progress_bar.update(len(render_view))
+        total_loss = total_loss / len(train_loader)
+        writer.add_scalar('Loss/train', total_loss, epoch)
+        
+        # Evaluation
+        model.eval()
+        val_losses = []
+        with torch.no_grad():
+            val_loss = 0
+            progress_bar = tqdm.tqdm(range(len(test_loader)))
+            for i, (render_view, base_color, normal, metallic, roughness, mask, recon_view) in enumerate(test_loader):
+                render_view = render_view.to(device).float()
+                base_color = base_color.to(device).float()
+                normal = normal.to(device).float()
+                metallic = metallic.to(device).float()
+                roughness = roughness.to(device).float()
+                mask = mask.to(device).float()
+                recon_view = recon_view.to(device).float()
+                
+                base_color = base_color*mask
+                normal = normal*mask
+                metallic = metallic*mask
+                roughness = roughness*mask
+                recon_view = recon_view*mask
+                
+                # Forward pass
+                base_color_pred, normal_pred, metallic_pred, roughness_pred = model(render_view)
+                base_color_pred = F.interpolate(base_color_pred, size=(512, 512), mode='bilinear', align_corners=False)
+                normal_pred = F.interpolate(normal_pred, size=(512, 512), mode='bilinear', align_corners=False)
+                metallic_pred = F.interpolate(metallic_pred, size=(512, 512), mode='bilinear', align_corners=False)
+                roughness_pred = F.interpolate(roughness_pred, size=(512, 512), mode='bilinear', align_corners=False)
+                
+                # Rendering
+                recon_view_pred = torch.zeros((base_color_pred.shape[0], 512, 512, 3), device=device).float()
+                for idx in range(len(base_color_pred)):
+                    recon_view_pred[idx] = render_torch(base_color_pred, metallic_pred, roughness_pred, normal_pred, light_dir, view_dir, idx)
+                recon_view_pred = recon_view_pred.permute(0, 3, 1, 2)
+                
+                # Compute loss
+                base_color_loss = torch.sqrt(criterion(base_color_pred, base_color))
+                normal_loss = torch.sqrt(criterion(normal_pred, normal))
+                metallic_loss = torch.sqrt(criterion(metallic_pred, metallic))
+                roughness_loss = torch.sqrt(criterion(roughness_pred, roughness))
+                rendering_loss = torch.sqrt(criterion(recon_view_pred, recon_view))
+                loss = (base_color_loss + normal_loss + metallic_loss + roughness_loss + rendering_loss)/5
+                
+                val_loss += loss.item()
+                progress_bar.update(len(render_view))
+                
+            val_loss = val_loss / len(test_loader)
+            print('Test - Epoch [{}/{}] Test Loss: {:.4f}'.format(epoch+1, num_epochs, val_loss))
+            writer.add_scalar('Loss/test', val_loss, epoch)
             
-            # Visualization
-            if vis:
-                visualize(render_view, base_color, normal, metallic, roughness, mask, recon_view,
-                        base_color_pred, normal_pred, metallic_pred, roughness_pred, recon_view_pred,
-                        mean, std)
+            if val_losses[-1] > val_loss:
+                torch.save(model.state_dict(), f'model_{epoch+1}_{val_loss}.pth')
+            val_losses.append(val_loss)
 
 train(True)
